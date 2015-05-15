197cc352f2801fdb0d908941443d46c43c0c2799
Salvatore Maletesta <salvatore.maletesta@gmail.com>
2015-05-15 12:48:19 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,108 @@
+var path = require('path');
+var http = require('http');
+
+var time = require('time');
+time.tzset('Europe/Rome');
+console.log("TIMEZONE=" + process.env.TZ)
+
+//var nko = require('nko')('R8N+nroFbZPS6D4n');
+var express = require('express');
+//var ejs = require('ejs');
+//var io = require('socket.io');
+
+
+//var events = require(path.join(__dirname,"lib","event-simulator","event-simulator.js"));
+var gtfsEvents = require(path.join(__dirname,"lib","gtfs-parser","gtfs-timetable-parser.js"));
+var mapDataGenerator = require(path.join(__dirname,"lib","map-data-generator","map-data-generator.js"));
+var PathNormalizer = require(path.join(__dirname,"lib","path-normalizer","path-normalizer.js"));
+var Gtfs = require(path.join(__dirname, "lib", "gtfs-parser", "gtfs-loader"));
+
+var app = express();
+
+var server = http.createServer(app)
+var io = require('socket.io').listen(server);
+
+
+app.configure(function() {
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.logger());
+	app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+	app.set('views', __dirname + '/views');
+	app.engine('.html', require('ejs').__express);
+//	app.register('.html', ejs);
+	app.set('view engine', 'html');
+});
+
+var gtfsdir = "lecce";
+
+var gtfs = Gtfs(process.env.GTFS_PATH || path.join(__dirname,"gtfs",gtfsdir), function(gtfsData){
+
+	mapDataGenerator.gen(gtfsData, process.env.GTFS_PATH || path.join(__dirname,"gtfs",gtfsdir), function(mapData) {
+
+		//calculate normalized shapes
+		var pathNormalizer = PathNormalizer(mapData.getShapes());
+
+	//	console.dir(mapData.getTrips());
+
+		require(path.join(__dirname, '/routes/site'))(app, mapData.getStops(), mapData.getShapes(),mapData.getTrips());
+
+		io.sockets.on('connection', function (socket) {
+		});
+
+		/* event simulator, throws an event every 10 secs. */
+		gtfsEvents.init(gtfsData, 10000, function(data) {
+
+			var trips = data.trips;
+
+			var pushData = {};
+
+			for(var i in trips){
+				if(trips.hasOwnProperty(i)){
+					var delta = (trips[i].progressThen - trips[i].progressNow) / 10;
+					//console.log(delta);
+					var pointList = [];
+
+					var shapeId = mapData.getShapeIdFromTripId(i);
+					if (!shapeId) continue;
+
+					for(var j = 0;j<10;j++){
+						var idx = Math.floor((trips[i].progressNow + j*delta)*1000);
+						if(idx === 1000 || idx ===0){
+							pointList.push([0,0]);
+						}
+						else{
+							pointList.push(pathNormalizer.getNormalizedPath(shapeId)[idx]);
+						}
+					}
+					pushData[i] = pointList;
+				}
+			}
+
+			/*
+			var p = Math.floor(step.progress * 10);
+
+			step['pointList'] = [];
+			step['foo'] = p;
+			for(var i = 0;i<10;i++){
+				step['pointList'].push(pathNormalizer.getNormalizedPath("87001")[p+i]);
+
+			}
+			*/
+			io.sockets.emit('event', pushData);
+		});
+
+		//server.listen(process.env.PORT || 8080);
+		//var appServer = app.listen(parseInt(process.env.PORT) || 7777);
+		//console.log('Listening on ' + server.address().port);
+
+		var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+		var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+		server.listen(server_port, server_ip_address, function(){
+  			console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+		});
+
+	});
+
+
+});
